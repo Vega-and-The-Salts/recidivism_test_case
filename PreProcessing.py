e9190c8eadd2f4c82439e469a60f21834148a4e8
Disguyranger <46687995+Disguyranger@users.noreply.github.com>
2022-07-12 17:43:54 -0400

    Add files via upload
    fixed class

--- PreProcessing.py
+++ PreProcessing.py
@@ -34,7 +34,7 @@ class PreProcess:
         Uses Seaborn
         :return:
         """
-
+        plt.figure(figsize=(13, 6))
         sns.heatmap(self.df.isnull(), yticklabels=False, cbar=False, cmap="Blues")
         return self.df.isnull()
 
@@ -48,20 +48,19 @@ class PreProcess:
         plt.figure(figsize=(8, 8))
         sns.heatmap(df.corr(), annot=False, cmap='BuPu')
         plt.show()
-        return df.corr()
+        return
 
-    def categorical_to_numeric(self):
+    def transform_categorical_to_numeric(self):
         df = self.df
         for col in df[df.select_dtypes(exclude=['int64', 'float64']).columns].columns.tolist():
             value = df[col].unique()
             for count, str_in_col in enumerate(value):
                 if str(str_in_col).lower() == 'nan':
-                    count = None
+                    count = -1
                 df[col] = df[col].replace(str_in_col, count)
-
         return df
 
-    def cleaning_dataframe(self, thresh_hold=.5, method='SI', mode='mean', state_or_neighbors=2):
+    def cleaning_dataframe(self, thresh_hold=.5, method="KNN", mode='mean', state_or_neighbors=2):
         """
         SimpleImputer is univariate, taking only a single feature into account.
         IterativeImputer takes a multivariate approach, or uses multiple features.
@@ -75,22 +74,25 @@ class PreProcess:
         :param mode:
         :return:
         """
+        df = self.df
         if method == 'SI':
             impute_data = SimpleImputer(missing_values=np.nan, strategy=mode, verbose=0)
         elif method == 'II':
             impute_data = IterativeImputer()
+        elif method == 'KNN':
+            impute_data = KNNImputer(n_neighbors=state_or_neighbors, weights='uniform', metric='nan_euclidean')
         else:
-            impute_data = KNNImputer(n_neighbors=state_or_neighbors)
-        df = self.df
-        for column in df:
-            if df[column].isnull().sum() / len(df) >= thresh_hold:
-                df.drop(columns=column, axis=1)
-            else:
-                impute_df = impute_data.fit_transform(df.select_dtypes(exclude=['str', 'obj']))
-
+            return "Method not provided."
+#        for column in list(df.columns):
+#            if df[column].isnull().sum() / len(df) >= thresh_hold:
+#                df = df.drop(columns=column, axis=1)
+#            else:
+#                df= impute_data.fit_transform(df.select_dtypes(include=['int64', 'float64']))
+        impute_data.fit(df)
+        df = impute_data.transform(df)
         return df
 
-    def train_dataset(self, column_name, tSize=.6, rStates=5):
+    def train_dataset(self, tSize=.6, rStates=5):
         """
         Takes in class information, currently dataframe.
         column_name is the column you want to predict.
@@ -102,6 +104,13 @@ class PreProcess:
         :return:
         """
         df = self.df
-        X_train, X_test, y_train, y_test = train_test_split(df.iloc[:-1], df.iloc[-1], test_size=tSize, random_state=rStates)
+        X_train, X_test, y_train, y_test = train_test_split(df.iloc[:, :-1], df.iloc[:, -1], test_size=tSize, random_state=rStates)
 
         return X_train, X_test, y_train, y_test
+
+    def print_head(self):
+        return self.df
+
+    def data_types(self):
+        df = self.df
+        return df.dtypes
